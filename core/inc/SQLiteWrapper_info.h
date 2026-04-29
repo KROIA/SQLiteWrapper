81a092ce6181864014d32f11d93c25e1930e7c57
Alex <alexkrieg@gmx.ch>
2026-04-30 01:04:43 +0200

    ~ Update Library Template 1.6.1

--- core/inc/SQLiteWrapper_info.h
+++ core/inc/SQLiteWrapper_info.h
@@ -6,6 +6,7 @@
 
 #include "SQLiteWrapper_global.h"
 #include "SQLiteWrapper_debug.h"
+#include "SQLiteWrapper_meta.h"
 
 #include <sstream>
 
@@ -53,15 +54,15 @@ namespace SQLiteWrapper
 		};
 
 
-		// Current version of the library
-		static constexpr int versionMajor				= 0;
-		static constexpr int versionMinor				= 0;
-		static constexpr int versionPatch				= 0;
+		// Current version of the library — driven by LIBRARY_VERSION in CMakeLists.txt
+		static constexpr int versionMajor				= SQLiteWrapper_VERSION_MAJOR;
+		static constexpr int versionMinor				= SQLiteWrapper_VERSION_MINOR;
+		static constexpr int versionPatch				= SQLiteWrapper_VERSION_PATCH;
 
 		static constexpr Version version{ versionMajor, versionMinor, versionPatch };
 
-		// Library name
-		static constexpr const char* name				= "SQLiteWrapper";
+		// Library name — driven by LIBRARY_NAME in CMakeLists.txt
+		static constexpr const char* name				= SQLiteWrapper_LIBRARY_NAME;
 		static constexpr const char* author				= "Alex Krieg";
 		static constexpr const char* email				= "";
 		static constexpr const char* website			= "https://www.sqlite.org/index.html";
